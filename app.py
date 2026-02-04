d2683902ec265f4a9824812d9891f542c688187d
Pedro Lucas <pedrolucasmoreira123321@gmail.com>
2026-02-04 20:51:28 -0300

    feat: rota para login criada

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 def index():
-    return render_template("login.html")
+    return redirect(url_for('login'))
+                    
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return "Faz login"
+    else:
+        return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
